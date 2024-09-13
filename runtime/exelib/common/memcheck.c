faed1b531353275b798969ea751fc9abd0e17f63
Luke Li <luke.li@ibm.com>
2024-09-13 12:46:53 -0400

    memoryCheck infinite loop bug fix

--- runtime/exelib/common/memcheck.c
+++ runtime/exelib/common/memcheck.c
@@ -730,7 +730,7 @@ static void OMRNORETURN memoryCheck_abort(OMRPortLibrary *portLib)
 	memCheckPortLib->tty_printf( memCheckPortLib, "Memory error(s) discovered, calling exit(3)\n");
 	memCheckPortLib->exit_shutdown_and_exit(memCheckPortLib, 3);
 
-for (;;)
+for (;;);
 /*dontreturn: goto dontreturn;  avoid warnings */
 }
 
