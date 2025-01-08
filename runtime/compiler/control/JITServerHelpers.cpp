1861e00dfec2711a9b8b2b66dd786ea0d537ac95
Luke Li <luke.li@ibm.com>
2025-01-08 15:43:26 -0500

    post segfault print

--- runtime/compiler/control/JITServerHelpers.cpp
+++ runtime/compiler/control/JITServerHelpers.cpp
@@ -1014,6 +1014,7 @@ JITServerHelpers::packRemoteROMClassInfo(J9Class *clazz, J9VMThread *vmThread, T
    TR_J9VM *fe = (TR_J9VM *)TR_J9VMBase::get(vmThread->javaVM->jitConfig, vmThread);
    printf("LL prrci %p %p\n", clazz, clazz->eyecatcher);
    J9Method *methodsOfClass = (J9Method *)fe->getMethods((TR_OpaqueClassBlock *)clazz);
+   printf("LL fprrci %p %p\n", clazz, clazz->eyecatcher);
    int32_t numDims = 0;
    TR_OpaqueClassBlock *baseClass = fe->getBaseComponentClass((TR_OpaqueClassBlock *)clazz, numDims);
    TR_OpaqueClassBlock *parentClass = fe->getSuperClass((TR_OpaqueClassBlock *)clazz);
