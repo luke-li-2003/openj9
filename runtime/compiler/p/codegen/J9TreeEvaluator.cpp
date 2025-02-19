b32170e4425b226475885eb0b61593790d6e1fa7
Luke Li <luke.li@ibm.com>
2025-02-19 11:35:06 -0500

    works hopefully

--- runtime/compiler/p/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/p/codegen/J9TreeEvaluator.cpp
@@ -11012,7 +11012,6 @@ hashCodeHelper(TR::Node *node, TR::CodeGenerator *cg, TR::DataType elementType,
    cg->stopUsingRegister(vtmp2Reg);
    cg->stopUsingRegister(vconstant0Reg);
    cg->stopUsingRegister(vconstantNegReg);
-   cg->stopUsingRegister(vunpackMaskReg);
 
    cg->stopUsingRegister(low4Reg);
    switch (elementType)
@@ -11024,6 +11023,7 @@ hashCodeHelper(TR::Node *node, TR::CodeGenerator *cg, TR::DataType elementType,
          // fall through
       case TR::Int16:
          cg->stopUsingRegister(high4Reg);
+         cg->stopUsingRegister(vunpackMaskReg);
          // fall through
       case TR::Int32:
          break;
